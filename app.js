2f7f2d7ba7f4f85e18eaa2a3ab35ac246de3ab2f
Maruf <alenmaruf.am@gmail.com>
2022-02-02 03:25:14 +0600

    Importing npm Modules(Validator)

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const getNotes = require('./notes.js')
+const validator = require("validator");
+const getNotes = require("./notes.js");
 
-const msg = getNotes()
+const msg = getNotes();
+console.log(msg);
 
-console.log(msg)
+console.log(validator.isEmail("alenmar.am@gmail.com"));
+console.log(validator.isEmail("alenmar.amgmail.com"));
+console.log(validator.isURL("https://comila.com"));
+console.log(validator.isURL("https:/comila.com"));
